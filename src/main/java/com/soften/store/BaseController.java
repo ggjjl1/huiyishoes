336d7c4511bedddcde75911755af3d5a65fc9fdb
ggjjl1 <ggjjl1@126.com>
2016-02-04 19:30:57 +0800

    add config

--- src/main/java/com/soften/store/BaseController.java
+++ src/main/java/com/soften/store/BaseController.java
@@ -1,5 +1,6 @@
 package com.soften.store;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,10 +8,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Created by ggjjl1 on 12/25/15.
  */
+@Controller
+@RequestMapping("/hello")
 public class BaseController {
     @RequestMapping(value="/", method= RequestMethod.GET)
-    public String hello(ModelMap model){
+    public String index(ModelMap model){
         model.addAttribute("message", "hello world!");
         return "index";
+    }
+
+    @RequestMapping(value="/hello")
+    public String hello(){
+        return "hello";
     }
 }
